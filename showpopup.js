d23aa7907a54a888de304763f7d02d253ee259cf
td <jmdnnlln@live.com>
2021-04-22 14:37:44 +0100

    feat(dev-tools): Add dark mode & restructure files

--- showpopup.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  $("#gh_button").click(function() {
-    var search_term = $("#gh_search").val();
-    chrome.tabs.create({url:'https://github.com/search?q=org%3APlayerData+' + search_term});
-  });
-});
