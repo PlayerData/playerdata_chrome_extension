a68a0a1240066bd8a0eaea291a149a833eee201d
Daniel Casserly <daniel@playerdata.com>
2022-12-02 16:59:55 +0000

    fix(github): remove points check

--- js/check-project-board.js
+++ js/check-project-board.js
@@ -20,43 +20,9 @@ const check_in_progress = () => {
     : area.classList.add("pd-in-progress-orange");
 };
 
-const check_for_points = () => {
-  const cards = document.querySelectorAll(
-    '[data-test-id="board-view-column-card"]'
-  );
-  cards.forEach((card) => {
-    const allLabels = card.querySelectorAll('[data-test-id="issue-label"]');
-    var isEpic = false;
-    var hasPoints = false;
-    allLabels.forEach((lbl) => {
-      if (lbl.firstChild.textContent === "epic") {
-        isEpic = true;
-      }
-      if (lbl.firstChild.textContent.indexOf("points/") > 0) {
-        hasPoints = true;
-      }
-    });
-
-    if (isEpic && !hasPoints) {
-      card.classList.add("pd-in-progress-red");
-    }
-  });
-};
-
-const handleScroll = () => {
-  document.querySelector('[data-test-id="board-view"]').addEventListener(
-    "scroll",
-    function (event) {
-      check_for_points();
-    },
-    false
-  );
-};
 
 window.addEventListener("load", function () {
   setTimeout(() => {
     check_in_progress();
-    check_for_points();
-    handleScroll();
   }, 1000);
 });
